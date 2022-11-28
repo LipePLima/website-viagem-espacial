62e6b3766c3293d0a73743c74d409dfab1187004
Felipe Lima <felipe.lima0160@gmail.com>
2022-11-28 20:36:20 -0300

    Corrigindo erro do body no mobile

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,21 @@ const btnMobile = document.querySelector('#btnMobile');
 
 btnMobile.addEventListener('click', function toggleMenu () {
     const nav = document.querySelector('nav');
-    nav.classList.toggle('active')
+    const li = document.querySelectorAll('.li');
+    const a  = document.querySelectorAll('.a');
 
-    this.classList.toggle('active')
+    li.forEach( el => {
+        el.style.transition = 'none'
+        el.classList.toggle('active2')
+        setTimeout( () => el.style.transition = '1s', 5)
+    })
+
+    a.forEach( el => {
+        el.style.transition = 'none'
+        el.classList.toggle('active3')
+        setTimeout( () => el.style.transition = '1s', 5)
+    })
+
+    nav.classList.toggle('active')
+    this.classList.toggle('active1')
 })
